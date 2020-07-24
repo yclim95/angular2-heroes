254aecd087637a5d6523c85793c27a50a1e713e3
yclim95 <lyc950421@gmail.com>
2020-07-24 18:26:16 +0800

    Change to manual routing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,6 @@ import { MessagesComponent } from './messages/messages.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent, HeroesComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
