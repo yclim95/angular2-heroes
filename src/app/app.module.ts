1e7de48bc923b5960668b18a479c199b204f7978
yclim95 <lyc950421@gmail.com>
2020-07-24 18:28:53 +0800

    Remove angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -17,7 +16,6 @@ import { MessagesComponent } from './messages/messages.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
